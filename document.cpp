d4f63bc665670548e7b05e58090f1789756007a3
YujinKat <153272854+YujinKat@users.noreply.github.com>
2024-02-26 16:11:42 +0900

    Update document.cpp

---
+++ document.cpp
@@ -0,0 +1,11 @@
+#include "document.h"
+
+#include <utility>
+
+Document::Document(int id, double relevance, int rating)
+        : id(id)
+        , relevance(relevance)
+        , rating(rating) {
+    }
+
+    
